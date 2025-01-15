privacy-scaling-explorations/sonobe

Add `CycleFoldCommittedInstanceVar::new_incoming_from_components` to simplify the construction of incoming CF instances

folding-schemes/src/folding/circuits/cyclefold.rs
@@ -15,12 +15,12 @@ use ark_r1cs_std::{
 use ark_relations::r1cs::{
     ConstraintSynthesizer, ConstraintSystem, ConstraintSystemRef, Namespace, SynthesisError,
 };
-use ark_std::fmt::Debug;
-use ark_std::rand::RngCore;
-use ark_std::Zero;
-use core::{borrow::Borrow, marker::PhantomData};
+use ark_std::{borrow::Borrow, fmt::Debug, marker::PhantomData, rand::RngCore, One, Zero};
 
-use super::{nonnative::uint::NonNativeUintVar, CF1, CF2};
+use super::{
+    nonnative::{affine::NonNativeAffineVar, uint::NonNativeUintVar},
+    CF1, CF2,
+};
 use crate::arith::r1cs::{extract_w_x, R1CS};
 use crate::commitment::CommitmentScheme;
 use crate::constants::NOVA_N_BITS_RO;
@@ -46,6 +46,7 @@ where
     pub cmW: GC,
     pub x: Vec<NonNativeUintVar<CF2<C>>>,
 }
+
 impl<C, GC> AllocVar<CycleFoldCommittedInstance<C>, CF2<C>> for CycleFoldCommittedInstanceVar<C, GC>
 where
     C: CurveGroup,
@@ -124,6 +125,45 @@ where
             cmW_elems,
         ]
         .concat())
+    }
+}
+
+impl<C2, GC2> CycleFoldCommittedInstanceVar<C2, GC2>
+where
+    C2: CurveGroup,
+    GC2: CurveVar<C2, CF2<C2>> + ToConstraintFieldGadget<CF2<C2>>,
+    C2::BaseField: PrimeField,
+    for<'a> &'a GC2: GroupOpsBounds<'a, C2, GC2>,
+{
+    /// Creates a new `CycleFoldCommittedInstanceVar` from the given components.
+    pub fn new_incoming_from_components<C1: CurveGroup<ScalarField = C2::BaseField>>(
+        cmW: GC2,
+        r_bits: &[Boolean<CF2<C2>>],
+        points: Vec<NonNativeAffineVar<C1>>,
+    ) -> Result<Self, SynthesisError> {
+        // Construct the public inputs `x` from `r_bits` and `points`.
+        // Note that the underlying field can only safely store
+        // `CF1::<C2>::MODULUS_BIT_SIZE - 1` bits, but `r_bits` may be longer
+        // than that.
+        // Thus, we need to chunk `r_bits` into pieces and convert each piece
+        // to a `NonNativeUintVar`.
+        let x = r_bits
+            .chunks(CF1::<C2>::MODULUS_BIT_SIZE as usize - 1)
+            .map(|bits| {
+                let mut bits = bits.to_vec();
+                bits.resize(CF1::<C2>::MODULUS_BIT_SIZE as usize, Boolean::FALSE);
+                NonNativeUintVar::from(&bits)
+            })
+            .chain(points.into_iter().flat_map(|p| [p.x, p.y]))
+            .collect::<Vec<_>>();
+        Ok(Self {
+            // `cmE` is always zero for incoming instances
+            cmE: GC2::zero(),
+            // `u` is always one for incoming instances
+            u: NonNativeUintVar::new_constant(ConstraintSystemRef::None, CF1::<C2>::one())?,
+            cmW,
+            x,
+        })
     }
 }
 

privacy-scaling-explorations/folding-scheme

folding-schemes/src/folding/circuits/cyclefold.rs
@@ -15,12 +15,12 @@ use ark_r1cs_std::{
 use ark_relations::r1cs::{
     ConstraintSynthesizer, ConstraintSystem, ConstraintSystemRef, Namespace, SynthesisError,
 };
-use ark_std::fmt::Debug;
-use ark_std::rand::RngCore;
-use ark_std::Zero;
-use core::{borrow::Borrow, marker::PhantomData};
+use ark_std::{borrow::Borrow, fmt::Debug, marker::PhantomData, rand::RngCore, One, Zero};
 
-use super::{nonnative::uint::NonNativeUintVar, CF1, CF2};
+use super::{
+    nonnative::{affine::NonNativeAffineVar, uint::NonNativeUintVar},
+    CF1, CF2,
+};
 use crate::commitment::CommitmentScheme;
 use crate::constants::NOVA_N_BITS_RO;
 use crate::folding::nova::nifs::{nova::NIFS, NIFSTrait};
@@ -65,6 +65,7 @@ pub struct CycleFoldCommittedInstanceVar<C: Curve> {
     pub cmW: C::Var,
     pub x: Vec<NonNativeUintVar<CF2<C>>>,
 }
+
 impl<C: Curve> AllocVar<CycleFoldCommittedInstance<C>, CF2<C>>
     for CycleFoldCommittedInstanceVar<C>
 {
@@ -126,6 +127,39 @@ impl<C: Curve> AbsorbNonNativeGadget<C::BaseField> for CycleFoldCommittedInstanc
             cmW_elems,
         ]
         .concat())
+    }
+}
+
+impl<C2: Curve> CycleFoldCommittedInstanceVar<C2> {
+    /// Creates a new `CycleFoldCommittedInstanceVar` from the given components.
+    pub fn new_incoming_from_components<C1: Curve<ScalarField = C2::BaseField>>(
+        cmW: C2::Var,
+        r_bits: &[Boolean<CF2<C2>>],
+        points: Vec<NonNativeAffineVar<C1>>,
+    ) -> Result<Self, SynthesisError> {
+        // Construct the public inputs `x` from `r_bits` and `points`.
+        // Note that the underlying field can only safely store
+        // `CF1::<C2>::MODULUS_BIT_SIZE - 1` bits, but `r_bits` may be longer
+        // than that.
+        // Thus, we need to chunk `r_bits` into pieces and convert each piece
+        // to a `NonNativeUintVar`.
+        let x = r_bits
+            .chunks(CF1::<C2>::MODULUS_BIT_SIZE as usize - 1)
+            .map(|bits| {
+                let mut bits = bits.to_vec();
+                bits.resize(CF1::<C2>::MODULUS_BIT_SIZE as usize, Boolean::FALSE);
+                NonNativeUintVar::from(&bits)
+            })
+            .chain(points.into_iter().flat_map(|p| [p.x, p.y]))
+            .collect::<Vec<_>>();
+        Ok(Self {
+            // `cmE` is always zero for incoming instances
+            cmE: C2::Var::zero(),
+            // `u` is always one for incoming instances
+            u: NonNativeUintVar::new_constant(ConstraintSystemRef::None, CF1::<C2>::one())?,
+            cmW,
+            x,
+        })
     }
 }
 